7c73b32d891b4683acfecca3d2132be026499bbd
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-05-14 19:43:21 -0300

    feat: container

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,8 +1,35 @@
-const Hero = () => {
+import { styled, Grid, Container} from "@mui/material"
+import Avatar from "../../../../assets/image/avatar.jpeg"
 
-    return (
+const Hero = () => {
+  
+  
+  
+  const StyledHero = styled("div")(() => ({
+     backgroundColor: "black"
+  }))
+
+  const StyledImg = styled("img")(() => ({
+    width: "30%",
+    borderRadius: "50%"
+  }))
+
+    
+  
+  return (
       <>
-        Hero
+        <StyledHero>
+          <Container>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <StyledImg src={Avatar} />
+              </Grid>
+              <Grid item xs={8}>
+                xs=4
+              </Grid>
+            </Grid>
+          </Container>
+        </StyledHero>
       </>
     )
   }
