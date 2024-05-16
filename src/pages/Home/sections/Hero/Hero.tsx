9489297ef48aa365ff1bb16223cf484c6c021860
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-05-16 20:46:02 -0300

    refactor: update hero

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -21,16 +21,26 @@ const Hero = () => {
   return (
       <>
         <StyledHero>
-          <Container>
+          <Container maxWidth="lg">
             <Grid container spacing={2}>
-              <Grid item xs={4}>
+              <Grid item xs={12} md={4}>
                 <StyledImg src={Avatar} />
               </Grid>
-              <Grid item xs={8}>
+              <Grid item xs={12} md={8}>
                 <Typography color="primary" variant="h1" textAlign= "center">Gustavo Lima</Typography>
                 <Typography color="primary" variant="h2" textAlign= "center">Desenvolvedor Backend</Typography>
-                <Button>Download CV</Button>
-                <Button>Contato</Button>
+                <Grid container display='flex' justifyContent='center'>
+                  <Grid item xs={12} md={4} display='flex' justifyContent='center'>
+                    <Button>
+                      Download CV
+                    </Button>
+                  </Grid>
+                  <Grid item xs={12} md={4} display='flex' justifyContent='center'>
+                    <Button>
+                      Contato
+                    </Button>
+                  </Grid>
+                </Grid>
               </Grid>
             </Grid>
           </Container>
